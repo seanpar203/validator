7cc87501b604df258a8740999c8b50c5016d9cb2
Sean Parsons <seanpatrick2013@gmail.com>
2017-07-18 14:30:03 -0400

    Added Metaclass to simplify interface.

---
+++ validator/fields.py
@@ -0,0 +1,22 @@
+from collections import OrderedDict
+
+
+class DeclarativeFieldsMetaclass(type):
+	def __new__(mcs, name, bases, attrs):
+		# Collect fields from current class.
+		current_fields = []
+		for key, value in list(attrs.items()):
+			if isinstance(value, Field):
+				current_fields.append((key, value))
+				attrs.pop(key)
+		attrs['declared_fields'] = OrderedDict(current_fields)
+
+		new_class = super().__new__(mcs, name, bases, attrs)
+		return new_class
+
+
+class Field:
+	""" Validation Field. """
+
+	def __init__(self, validators=()) -> None:
+		self.validators = validators
