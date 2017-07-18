e49e4991c11d66f5074bc8c0186dae97cfcb325f
Sean Parsons <seanpatrick2013@gmail.com>
2017-07-18 14:29:36 -0400

    Created a BaseValidator class doing simple validation upon creation.

---
+++ validator/validator.py
@@ -0,0 +1,82 @@
+import copy
+from collections import defaultdict
+
+from typing import Any
+
+from validator.fields import Field, DeclarativeFieldsMetaclass
+
+__all__ = ('Validator',)
+
+
+class BaseValidator:
+	""" Base validation class, handles all logic. """
+
+	def __init__(self, data: dict) -> None:
+		self.data = data
+		self._errors = None
+		self.fields = copy.deepcopy(self.declared_fields)
+
+	# ------------------------------------------
+	# Properties
+	# ------------------------------------------
+
+	@property
+	def errors(self) -> defaultdict:
+		""" Returns dictionary with or without errors.
+
+		:return: Errors
+		:rtype: defaultdict
+		"""
+		if not self._errors:
+			self.full_validate()
+		return self._errors
+
+	# ------------------------------------------
+	# Public Methods
+	# ------------------------------------------
+
+	def is_valid(self) -> bool:
+		""" Returns if there is errors in validation.
+
+		:return: True or False
+		:rtype: bool
+		"""
+		return not self.errors
+
+	def full_validate(self):
+		""" Runs full validation against all defined Fields.
+
+		:return:
+		"""
+		self._errors = defaultdict(list)
+
+		for key, val in self.data.items():
+			try:
+				field = self.fields[key]
+			except KeyError:
+				continue
+			else:
+				self._validate_field(field, key, val)
+
+	def _validate_field(self, field: Field, key: str, val: Any) -> None:
+		""" Validates a field by running value through validators.
+
+		:param field: The validator field
+		:type field: Field
+
+		:param key: The key value for data & field.
+		:type key: str
+
+		:param val: The value of the data to pass into validators.
+		:type val: Any
+		"""
+		for validator in field.validators:
+			passed, err = validator(val)
+
+			if not passed:
+				self._errors[key].append(err)
+
+
+class Validator(metaclass=DeclarativeFieldsMetaclass, BaseValidator):
+	""" Class used to define custom Validator classes. """
+	pass
