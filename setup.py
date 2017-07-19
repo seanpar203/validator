37ab6cea8b0cef1ee98c760ede762d290f76600c
Sean Parsons <seanpatrick2013@gmail.com>
2017-07-19 14:21:04 -0400

    Added setup.py and LICENSE.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='dict-validator',
+    version='1.0.0',
+    packages=['validator'],
+    url='https://github.com/seanpar203/validator',
+    license='MIT',
+    author='Sean Parsons',
+    author_email='seanpatrick2013@gmail.com',
+    description='A simple way to validate dictionary values by using functions.',
+    download_url='https://github.com/seanpar203/validator/archive/1.0.0.tar.gz'
+)
