ed9ecaaeeca54c73cc9ab4531ed525304a7e0df2
Sean Parsons <seanpatrick2013@gmail.com>
2017-07-19 11:46:16 -0400

    Add doctstring to test.

--- tests/test_fields_validators.py
+++ tests/test_fields_validators.py
@@ -5,6 +5,11 @@ from .utils.validator_functions import TestValidators
 def test_field_validator_functions(user_validator):
     """
     This test makes sure that the defined functions are present.
+
+    Note:
+        Unfortunately for our dynamic functions like `is_gt_than` we can't
+        check because it returns a closure, but just knowing that the static
+        functions are present in the fields validators list is good enough.
     """
     email_field = user_validator.fields['email']
 
