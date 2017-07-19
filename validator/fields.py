c5a05010e5213b1f82e780be57d2a6d3501a9904
Sean Parsons <seanpatrick2013@gmail.com>
2017-07-19 10:27:43 -0400

    Added data_type to Field to validate a fields value before passing into validators.

--- validator/fields.py
+++ validator/fields.py
@@ -3,44 +3,49 @@ from collections import OrderedDict
 
 
 class DeclarativeFieldsMetaclass(type):
-	"""
-	Metaclass for removing declared Fields from Validator instances.
-	"""
+    """
+    Metaclass for removing declared Fields from Validator instances.
+    """
 
-	def __new__(mcs, name, bases, attrs):
-		# Collect fields from current class.
-		current_fields = []
-		for key, value in list(attrs.items()):
-			if isinstance(value, Field):
-				current_fields.append((key, value))
-				attrs.pop(key)
-		attrs['declared_fields'] = OrderedDict(current_fields)
+    def __new__(mcs, name, bases, attrs):
+        # Collect fields from current class.
+        current_fields = []
+        for key, value in list(attrs.items()):
+            if isinstance(value, Field):
+                current_fields.append((key, value))
+                attrs.pop(key)
 
-		new_class = super().__new__(mcs, name, bases, attrs)
-		return new_class
+        new_class = super().__new__(mcs, name, bases, attrs)
+        new_class.fields = OrderedDict(current_fields)
+        return new_class
 
 
 class Field:
-	""" Validation Field. """
+    """ Validation Field. """
 
-	def __init__(self, validators: tuple = ()) -> None:
-		self.validators = validators
+    def __init__(self, data_type: type = None, validators: list = []) -> None:
+        self.data_type = data_type
+        self.validators = validators
 
-	def validate(self, val: Any) -> list:
-		""" Validates value by passing into all validators
+    def validate(self, val: Any) -> list:
+        """ Validates value by passing into all validators
 
-		:param val: Value to pass into validators
-		:type val: Any
+        :param val: Value to pass into validators
+        :type val: Any
 
-		:return: Errors or empty list.
-		:rtype: list
-		"""
-		errors: list = []
+        :return: Errors or empty list.
+        :rtype: list
+        """
+        errors: list = []
 
-		for validator in self.validators:
-			passed, err = validator(val)
+        if self.data_type and type(val) != self.data_type:
+            errors.append(f"{val} is expected to be a {self.data_type}")
+            return errors
 
-			if not passed:
-				errors.append(err)
+        for validator in self.validators:
+            passed, err = validator(val)
 
-		return errors
+            if not passed:
+                errors.append(err)
+
+        return errors
