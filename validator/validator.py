e17ce4bef4904dc892cf9dba14654763dad1157f
Sean Parsons <seanpatrick2013@gmail.com>
2017-07-19 10:28:52 -0400

    Removed declaring self.fields in BaseValidator since it's done in Metaclass.

--- validator/validator.py
+++ validator/validator.py
@@ -4,56 +4,55 @@ from validator.fields import Field, DeclarativeFieldsMetaclass
 
 
 class BaseValidator:
-	""" Base validation class, handles all logic. """
+    """ Base validation class, handles all logic. """
 
-	def __init__(self, data: dict) -> None:
-		self.data = data
-		self._errors: DefaultDict[str, list] = defaultdict(list)
-		self.fields: Dict[str, Field] = self.declared_fields  # type: ignore
+    def __init__(self, data: dict) -> None:
+        self.data = data
+        self._errors: DefaultDict[str, list] = defaultdict(list)
 
-	# ------------------------------------------
-	# Properties
-	# ------------------------------------------
+    # ------------------------------------------
+    # Properties
+    # ------------------------------------------
 
-	@property
-	def errors(self) -> defaultdict:
-		""" Returns dictionary with or without errors.
+    @property
+    def errors(self) -> defaultdict:
+        """ Returns dictionary with or without errors.
 
-		:return: Errors
-		:rtype: defaultdict
-		"""
-		if not self._errors:
-			self.full_validate()
-		return self._errors
+        :return: Errors
+        :rtype: defaultdict
+        """
+        if not self._errors:
+            self.full_validate()
+        return self._errors
 
-	# ------------------------------------------
-	# Public Methods
-	# ------------------------------------------
+    # ------------------------------------------
+    # Public Methods
+    # ------------------------------------------
 
-	def is_valid(self) -> bool:
-		""" Returns if there is errors in validation.
+    def is_valid(self) -> bool:
+        """ Returns if there is errors in validation.
 
-		:return: True or False
-		:rtype: bool
-		"""
-		return not self.errors
+        :return: True or False
+        :rtype: bool
+        """
+        return not self.errors
 
-	def full_validate(self) -> None:
-		""" Runs full validation against all defined Fields. """
-		self._errors: DefaultDict[str, list] = defaultdict(list)
+    def full_validate(self) -> None:
+        """ Runs full validation against all defined Fields. """
+        self._errors: DefaultDict[str, list] = defaultdict(list)
 
-		for key, val in self.data.items():
-			try:
-				field: Field = self.fields[key]
-			except KeyError:
-				continue
-			else:
-				errors: list = field.validate(val)
+        for key, val in self.data.items():
+            try:
+                field: Field = self.fields[key]
+            except KeyError:
+                continue
+            else:
+                errors: list = field.validate(val)
 
-				if errors:
-					self._errors[key].append(err for err in errors)
+                if errors:
+                    self._errors[key].append(err for err in errors)
 
 
 class Validator(BaseValidator, metaclass=DeclarativeFieldsMetaclass):
-	""" Class used to define custom Validator classes. """
-	pass
+    """ Class used to define custom Validator classes. """
+    pass
