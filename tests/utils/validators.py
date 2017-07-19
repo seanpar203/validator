9be8a795d55f27bac581545d5a69ffa1793c223e
Sean Parsons <seanpatrick2013@gmail.com>
2017-07-19 11:31:41 -0400

    Added file to validators to use for testing.

---
+++ tests/utils/validators.py
@@ -0,0 +1,21 @@
+from validator import Validator, Field
+
+from .validator_functions import TestValidators
+
+
+class UserValidator(Validator):
+    """
+    User Test validator.
+    """
+    email = Field(
+        data_type=str,
+        validators=[TestValidators.is_email]
+    )
+    age = Field(
+        data_type=int,
+        validators=[TestValidators.is_gt_than(10)]
+    )
+    height = Field(
+        data_type=int,
+        validators=[TestValidators.is_gt_but_lt(50, 90)]
+    )
